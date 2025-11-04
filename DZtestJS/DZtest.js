3dd00d3e674c97e1ddfcde45ce32f27d095c8bed
Anya-Kirshteyn <ann65ann@gmail.com>
2025-11-04 11:05:42 +0200

    rework HW

--- DZtestJS/DZtest.js
+++ DZtestJS/DZtest.js
@@ -786,5 +786,121 @@
 // renderPage();
 
 
+// #HmvAfRQM
+//
 // – взяти https://dummyjson.com/docs/carts та вивести інформацію про всі корзини. Відобразити всі поля кожної корзини.
+//
+// let divContainer=document.getElementById('containerDiv')
+//
+// fetch('https://dummyjson.com/carts')
+//     .then(carts => carts.json())
+//     .then(cartsObj=>{
+//         const {carts}=cartsObj;
+//
+//         for(let cart of carts){
+//     let productDiv =document.createElement("div")
+//             productDiv.innerText=
+//                 `
+//             id:${cart.id}
+//             total:${cart.total}
+//             discountedTotal: ${cart.discountedTotal}
+//       userId: ${cart.userId},
+//       totalProducts: ${cart.totalProducts}
+//       totalQuantity:${cart.totalQuantity}
+//                `
+//   for(let product of cart.products){
+//       let img=document.createElement("img")
+//       img.src=product.thumbnail
+// productDiv.appendChild(img)
+//
+//     let ul=document.createElement("ul")
+//       productDiv.appendChild(ul)
+//       let li=document.createElement("li")
+//       ul.appendChild(li)
+//       li.innerText=`
+//       id:${product.id}
+//       "title": ${product.title}
+//           "price": ${product.price}
+//           "quantity": ${product.quantity}
+//           "total": ${product.total}
+//           "discountPercentage": ${product.discountPercentage}
+//           "discountedTotal": ${product.discountedTotal}
+//
+//
+//       `
+//   }
+//
+//     divContainer.appendChild(productDiv)
+//
+//     }
+//     })
+
+// – взяти https://dummyjson.com/docs/recipes та вивести інформацію про всі рецепти. Інгредієнти повинні бути списком під час відображення.
+// let containerDiv=document.getElementById('containerDiv')
+// fetch('https://dummyjson.com/recipes')
+//     .then(res => res.json())
+//     .then(recipesObj=> {
+//         const {recipes} = recipesObj;
+//
+//         for (let recipe of recipes) {
+//             // console.log(recipe);
+// let recipeDivHead = document.createElement('div');
+// containerDiv.appendChild(recipeDivHead);
+// recipeDivHead.innerText=`
+// id:${recipe.id}
+// name: ${recipe.name}
+// `
+//
+//       let ol=document.createElement("ol");
+//       recipeDivHead.appendChild(ol);
+//
+// for (let ingredient of recipe.ingredients) {
+//     let li=document.createElement("li");
+//     li.innerText=ingredient;
+//     ol.appendChild(li);
+//
+// }
+// let recipeDivBody=document.createElement("div");
+// containerDiv.appendChild(recipeDivBody);
+//
+//
+// recipeDivBody.innerText=`
+// instructions:${recipe.instructions}
+// prepTimeMinutes: ${recipe.prepTimeMinutes},
+//       cookTimeMinutes: ${recipe.cookTimeMinutes},
+//       servings: ${recipe.servings},
+//       difficulty: ${recipe.difficulty},
+//       cuisine: ${recipe.cuisine}
+//       caloriesPerServing:${recipe.caloriesPerServing}
+//       tags:${recipe.tags}
+// "userId": ${recipe.userId},
+//
+//       "rating": ${recipe.rating}
+//       "reviewCount": ${recipe.reviewCount}
+//       "mealType": ${recipe.mealType}
+//
+//
+// `
+//             let img=document.createElement("img");
+//             img.src=recipe.image
+//             recipeDivBody.appendChild(img);
+//
+//         }
+//
+//     });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
